f9ccb9b501cff4e0855f80f2c05377499f9e59dc
Istudiohtml <istudiohtml@gmail.com>
2022-11-16 14:17:31 +0700

    fixed update

--- app/Http/Controllers/main/CampaignController.php
+++ app/Http/Controllers/main/CampaignController.php
@@ -18,6 +18,17 @@ class CampaignController extends Controller
         $data = [];
         foreach ($campaigns as $campaign) {
             $campaign->keyword = Keyword::where('campaign_id', $campaign->id)->get();
+
+            if ($campaign->keyword) {
+                foreach ($campaign->keyword as $item) {
+                    $item->keyword_or = explode(",", $item->keyword_or);
+                    $item->keyword_and = explode(",", $item->keyword_and);
+                    $item->keyword_exclude = explode(",", $item->keyword_exclude);
+                }
+//                $item->keyword_or = explode(",",$item->keyword_or)();
+//                $item->keyword_and = json_decode($item->keyword_and);
+//                $item->keyword_exclude = json_decode($item->keyword_exclude);
+            }
             $campaign->organization = Organization::find($campaign->organization_id)->name;
             $data[] = $campaigns;
 
@@ -51,7 +62,6 @@ class CampaignController extends Controller
 
         $campaign = Campaign::create($data_submit);
 
-
         if ($request->keywords) {
             foreach ($request->keywords as $keyword) {
 
@@ -76,21 +86,64 @@ class CampaignController extends Controller
 
     }
 
-    public function update(Request $request, $action = null)
+    public function update(Request $request)
     {
         $id = $request->id;
-        try {
-            $res = $this->find($id);
-            if ($res[BaseModel::STATUS] !== 200) {
-                $permission = $res[BaseModel::DATA_TEXT];
 
-                if ($action === BaseModel::UPDATE_TEXT) {
-                    $permission->update($request->all());
-                    return parent::handleRespond($permission);
-                }
-                $permission->update([BaseModel::STATUS => false]);
-                return parent::handleRespond(null);
+        try {
+            $campaign= $this->find($id);
+
+            if ($campaign[BaseModel::STATUS] !== 200) {
+
+
+//                return
+//                if ($action === BaseModel::UPDATE_TEXT) {
+//                    $data->update($request->all());
+//                    return parent::handleRespond($data);
+//                }
+
+
+
+                return parent::handleRespond($data_submit);
             }
+
+            $data = $campaign[BaseModel::DATA_TEXT];
+
+            $data_submit = [
+                BaseModel::NAME => $request->name ?? '',
+                Campaign::DESCRIPTION => $request->description,
+                BaseModel::ORGANIZATION_ID => $request->organization_id ?? 1,
+                Campaign::DOMAIN_ID => $request->domain_id ?? 1,
+                BaseModel::STATUS => 1,
+                Campaign::EXCLUDE_CAMPAIGN => collect($request->exclude_campaign)->implode(','),
+                Campaign::START_AT => $request->start_at,
+                Campaign::END_AT => $request->end_at,
+            ];
+
+            $data->update($data_submit);
+
+
+            if ($request->keywords) {
+                foreach ($request->keywords as $keyword) {
+
+                    $data_submit_keyword = [
+                        Keyword::CAMPAIGN_ID => $data->id,
+                        BaseModel::NAME => $keyword[BaseModel::NAME] ?? '',
+                        Keyword::KEYWORD_OR => collect($keyword[Keyword::KEYWORD_OR] ?? [])->implode(','),
+                        Keyword::KEYWORD_AND => collect($keyword[Keyword::KEYWORD_AND] ?? [])->implode(','),
+                        Keyword::KEYWORD_EXCLUDE => collect($keyword[Keyword::KEYWORD_EXCLUDE] ?? [])->implode(','),
+                        BaseModel::STATUS => 1,
+                        BaseModel::CREATED_BY => auth('api')->id() ?? 1,
+                        BaseModel::UPDATED_BY => auth('api')->id() ?? 1,
+                    ];
+
+                    Keyword::updateOrCreate([ 'id' => $keyword['id'] ?? null], $data_submit_keyword);
+                }
+            }
+
+
+
+            return parent::handleRespond($this->find($id));
 
         } catch (Exception $exception) {
             return parent::handleErrorRespond($exception, $exception->getCode());
@@ -108,8 +161,8 @@ class CampaignController extends Controller
             BaseModel::STATUS => 404,
             BaseModel::MSG_TEXT => BaseModel::NOT_FOUND_TEXT
         ];
-
-        if ($id && $campaign = Campaign::find($id)) {
+        $campaign = Campaign::find($id);
+        if ($id && $campaign) {
             $res[BaseModel::STATUS] = 200;
             $res[BaseModel::DATA_TEXT] = $campaign;
             return $res;
