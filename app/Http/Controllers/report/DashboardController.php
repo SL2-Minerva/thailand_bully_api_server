ed2d57120b23d2d52dbf334fed76a54d71dceb97
Istudiohtml <istudiohtml@gmail.com>
2022-12-14 14:18:24 +0700

    fixed show new data of dashboard

--- app/Http/Controllers/report/DashboardController.php
+++ app/Http/Controllers/report/DashboardController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\report;
 
 use App\Http\Controllers\Controller;
-use App\Models\BaseModel;
 use App\Models\DailyMessage;
 use App\Models\PercentageOfMessages;
 use Carbon\Carbon;
@@ -23,10 +22,10 @@ class DashboardController extends Controller
         $period = $this->get_period($request->period);
         $data['daily_message'] = $this->dailyMessage($campaign_id, '2022-11-30', '2022-11-30');
         $data['prcentage_of_messages_current'] = $this->percentageOfMessages($campaign_id, '2022-11-30', '2022-11-30');
-        $data['prcentage_of_messages_previous'] = $this->percentageOfMessages(
-            $campaign_id, $this->get_previous_date('2022-11-30', $period),
-            $this->get_previous_date('2022-11-30', $period)
-        );
+//        $data['prcentage_of_messages_previous'] = $this->percentageOfMessages(
+//            $campaign_id, $this->get_previous_date('2022-11-30', $period),
+//            $this->get_previous_date('2022-11-30', $period)
+//        );
 
         return parent::handleRespond($data);
     }
@@ -49,63 +48,57 @@ class DashboardController extends Controller
 
 
     private function dailyMessage($campaign_id, $start_date, $end_date) {
-
-//        $test = Carbon::parse($start_date)->format('Y-m-d');
-//        $test = Carbon::createFromFormat('Y-m-d', $start_date);
-
-        $daily_messages = DailyMessage::where('campaign_id', $campaign_id);
-
-        $daily_messages = $daily_messages->whereBetween('date_m', [$start_date, $end_date]);
-
-//        else {
-//            $daily_messages = $daily_messages->whereBetween('date', [$start_date, $end_date]);
-//        }
-
         $data = null;
+        $daily_messages = DailyMessage::where('campaign_id', $campaign_id);
+        $daily_messages = $daily_messages->whereBetween('date_m', [$start_date, $end_date]);
 
         foreach ($daily_messages->get() as $daily_message) {
 
-           $nestData = [
-                'keyword_id' => $daily_message->keyword_id,
-                'keyword_name' => $daily_message->keyword_name,
-                'campaign_id' => $daily_message->campaign_id,
-                'campaign_name' => $daily_message->campaign_name,
-                'organization_id' => $daily_message->organization_id,
-                'organizations_name' => $daily_message->organizations_name,
+            $keyword_id = $daily_message->keyword_id;
+            $data[$keyword_id]['keyword_id'] = $daily_message->keyword_id;
+            $data[$keyword_id]['keyword_name'] = $daily_message->keyword_name;
+            $data[$keyword_id]['campaign_id'] = $daily_message->campaign_id;
+            $data[$keyword_id]['campaign_name'] = $daily_message->campaign_name;
+            $data[$keyword_id]['organization_id'] = $daily_message->organization_id;
+            $data[$keyword_id]['organizations_name'] = $daily_message->organizations_name;
+
+            $nestData = [
                 'source_id' => $daily_message->source_id,
                 'source_name' => $daily_message->source_name,
                 'date_m' => $daily_message->date_m,
                 'total_at_date' => $daily_message->total_at_date
             ];
 
-           if (isset($data['keyword_id']) && $data['keyword_id'] === $daily_message->keyword_id) {
-                $data['data'][] = $nestData;
-           } else {
-               $data['keyword_id'] = $daily_message->keyword_id;
-               $data['data'][] = $nestData;
-           }
+            $data[$keyword_id]['value'][] = $nestData;
         }
 
-        return  $data;
+        return array_values($data);
     }
 
     private function percentageOfMessages($campaign_id, $start_date, $end_date) {
-
+        $data = null;
         $percentage_of_messages = PercentageOfMessages::where('campaign_id', $campaign_id);
-
         $percentage_of_messages->whereBetween('date_m', [$start_date, $end_date]);
+
         foreach ($percentage_of_messages->get() as $percentage_of_message) {
-            if (isset($data['keyword_id']) && $data['keyword_id'] === $percentage_of_message->keyword_id) {
-                $data['data']['percentage'] = (int)$data['data']['percentage'] + $percentage_of_message->total_at_keyword;
-            } else {
-                $data['keyword_id'] = $percentage_of_message->keyword_id;
-                $data['keyword_name'] = $percentage_of_message->keyword_name;
-                $data['date'] = $start_date .'-'. $end_date;
-                $data['data']['percentage'] = $percentage_of_message->total_at_keyword;
-            }
+            $keyword_id = $percentage_of_message->keyword_id;
+
+            $data[$keyword_id]['keyword_id'] = $percentage_of_message->keyword_id;
+            $data[$keyword_id]['keyword_name'] = $percentage_of_message->keyword_name;
+            $data[$keyword_id]['campaign_id'] = $percentage_of_message->campaign_id;
+            $data[$keyword_id]['campaign_name'] = $percentage_of_message->campaign_name;
+            $data[$keyword_id]['organization_id'] = $percentage_of_message->organization_id;
+            $data[$keyword_id]['organizations_name'] = $percentage_of_message->organizations_name;
+
+            $nestData = [
+                'date' => $start_date .' - '. $end_date,
+                'percentage' => $percentage_of_message->total_at_keyword
+            ];
+
+            $data[$keyword_id]['value'][] = $nestData;
         }
 
-        return [$data];
+        return array_values($data);
 
     }
 
