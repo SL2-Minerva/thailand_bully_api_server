4a4152203db50ad090e885d9c6e6dc2f1aa1a7e1
Istudiohtml <istudiohtml@gmail.com>
2023-02-21 16:56:44 +0700

    fixed wordcrow not working

--- app/Http/Controllers/report/DashboardController.php
+++ app/Http/Controllers/report/DashboardController.php
@@ -1331,11 +1331,11 @@ class DashboardController extends Controller
                 ->whereBetween('date_count', [$this->start_date, $this->end_date])->orderBy('count_number');
         }
 
-        if ($request->sentiment_type) {
-            $raw->where('word_clouds.classification_type_id', 1)
-            ->where('word_clouds.classification_id', 1);
-
-        }
+//        if ($request->sentiment_type) {
+//            $raw->where('word_clouds.classification_type_id', 1)
+//            ->where('word_clouds.classification_id', 1);
+//
+//        }
 
         if ($this->keyword_id) {
             $raw->whereIn('keyword_id', $this->keyword_id);
