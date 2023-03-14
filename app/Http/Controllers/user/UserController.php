d7785b9a1e757b00fb76754530dea96c305ee1be
Night Zung <night@spacereax.co>
2023-03-14 11:19:14 +0700

    Feature Forgot Password

--- app/Http/Controllers/user/UserController.php
+++ app/Http/Controllers/user/UserController.php
@@ -7,8 +7,13 @@ use App\Models\BaseModel;
 use App\Models\User;
 use App\Models\UserPermission;
 use App\Models\UserRole;
+use Carbon\Carbon;
+use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class UserController extends Controller
 {
@@ -140,5 +145,27 @@ class UserController extends Controller
         }
 
         return parent::handleRespond($user->get());
+    }
+
+    public function forget_password(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|exists:users',
+        ]);
+
+        $token = Str::random(64);
+
+        DB::table('password_resets')->insert([
+            'email' => $request->email, 
+            'token' => $token, 
+            'created_at' => Carbon::now()
+          ]);
+
+        Mail::send('auth.forget-password-email', ['token' => $token], function($message) use($request){
+            $message->to($request->email);
+            $message->subject('Reset Password');
+        });
+
+        // return parent::handleRespond($token);
     }
 }
