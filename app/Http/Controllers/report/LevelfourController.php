8a08a3cd332b345192e2414b1d205034fa01faed
Istudiohtml <istudiohtml@gmail.com>
2023-03-24 10:23:36 +0700

    test side level 4

--- app/Http/Controllers/report/LevelfourController.php
+++ app/Http/Controllers/report/LevelfourController.php
@@ -269,7 +269,7 @@ class LevelfourController extends Controller
 
 
 //            if ($is_child) {
-            $data_push["length"] = (int)$influent_rate <= 0 ? 10 : (int)$influent_rate + 10;
+            $data_push["length"] = (int)$influent_rate <= 0 ? 10 : (int)$influent_rate + 5;
             $data_push["parent_id"] = $item->reference_message_id;
 //            }
 
