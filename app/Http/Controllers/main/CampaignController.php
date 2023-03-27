ec5c0d62d18950eea142207bf932e84ada697b63
Istudiohtml <istudiohtml@gmail.com>
2023-03-27 10:48:16 +0700

    add frequency for org group

--- app/Http/Controllers/main/CampaignController.php
+++ app/Http/Controllers/main/CampaignController.php
@@ -443,6 +443,7 @@ class CampaignController extends Controller
             $campaign->organization = Organization::find($campaign->organization_id)->name;
             $data['list'][] = $campaign;
             $data['keyword_limit'] = $this->organization_group->total_keyword;
+            $data['frequency'] = $this->organization_group->frequency ?? 0;
 
 
 
