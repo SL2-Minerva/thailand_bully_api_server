4dd9dde93dcbd8127ffbe3eb4481b3fad4d79e42
Mezook <sirawit.mezook@gmail.com>
2024-01-23 19:19:05 +0700

    Add condition select only post Post / author

--- app/Http/Controllers/report/MonitoringController.php
+++ app/Http/Controllers/report/MonitoringController.php
@@ -595,6 +595,10 @@ class MonitoringController extends Controller
                     COALESCE(number_of_shares, 0) +
                     COALESCE(number_of_reactions, 0) AS total_engagement'))
             //->leftJoin('sources as s', 's.id', '=', 'messages.source_id')
+            ->where(function ($query) {
+                $query->where('message_type', '=', 'Post')
+                    ->orWhere('message_type', '=', 'post');
+            })
             ->whereIn('messages.keyword_id', $keywordIds)
             ->whereBetween('messages.message_datetime', [$this->start_date . " 00:00:00", $this->end_date . " 23:59:59"]);
 
