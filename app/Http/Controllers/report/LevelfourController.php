139d73874ef0a639940aeb15dc3992c2c1f0fc11
Mezook <sirawit.mezook@gmail.com>
2024-02-13 13:45:14 +0700

    Fixed

--- app/Http/Controllers/report/LevelfourController.php
+++ app/Http/Controllers/report/LevelfourController.php
@@ -205,7 +205,7 @@ class LevelfourController extends Controller
         if ($is_child) {
             $raw = $this->message()
                 ->where('message_results.classification_type_id', $type)
-                ->whereIn('messages.reference_message_id', $parentMessageIds)->limit(5000);
+                ->whereIn('messages.reference_message_id', $parentMessageIds)->limit(8000);
         } else {
             if ($message_id) {
                 $raw = $this->message();
@@ -214,7 +214,7 @@ class LevelfourController extends Controller
             } else {
                 $raw = $this->message_root($keywordIds, $this->start_date, $this->end_date)
                     ->where('message_results.classification_type_id', $type)
-                    ->where('messages.reference_message_id', '')->limit(500)
+                    ->where('messages.reference_message_id', '')->limit(700)
                     ->groupBy("messages.message_id");
             }
         }
