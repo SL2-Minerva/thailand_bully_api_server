c0aafe389b995d52e3c7a278685550fe59b64cfe
Mezook <sirawit.mezook@gmail.com>
2024-03-07 15:30:43 +0700

    Fixed

--- app/Http/Controllers/report/MonitoringController.php
+++ app/Http/Controllers/report/MonitoringController.php
@@ -62,24 +62,25 @@ class MonitoringController extends Controller
 
         $data = DB::table('messages')
             ->select([
-                'messages.id AS id',
-                'messages.author AS author',
-                'messages.message_id AS message_id',
-                'messages.reference_message_id AS reference_message_id',
-                'messages.keyword_id AS keyword_id',
+                'messages.id',
+                'messages.author',
+                'messages.message_id',
+                'messages.reference_message_id',
+                'messages.keyword_id',
                 'messages.message_datetime AS date_m',
-                'messages.source_id AS source_id',
-                'messages.full_message AS full_message',
+                'messages.source_id',
+                'messages.full_message',
                 'messages.message_type',
-                'messages.link_message AS link_message',
-                'messages.device AS device',
-                'messages.number_of_views AS number_of_views',
-                'messages.number_of_comments AS number_of_comments',
-                'messages.number_of_shares AS number_of_shares',
-                'messages.number_of_reactions AS number_of_reactions',
+                'messages.link_message',
+                'messages.link_image',
+                'messages.device',
+                'messages.number_of_views',
+                'messages.number_of_comments',
+                'messages.number_of_shares',
+                'messages.number_of_reactions',
                 /*'message_results.classification_id',
                 'message_results.classification_type_id',*/
-                'messages.created_at AS created_at',
+                'messages.created_at',
                 /*'keywords.name AS keyword_name',
                 'keywords.campaign_id AS campaign_id',
                 'campaigns.name AS campaign_name',
@@ -352,7 +353,7 @@ class MonitoringController extends Controller
         $raw_data = $raw->orderByDesc('total_engagement')->limit(6)->get();
         $source = $this->getAllSource();
         $data = array();
-        error_log($raw->toSql());
+        //error_log($raw->toSql());
 
         foreach ($raw_data as $item) {
 
@@ -377,6 +378,7 @@ class MonitoringController extends Controller
                 "device" => $item->device,
                 "source_name" => parent::matchSourceName($source, $item->source_id),
                 "link_message" => $item->link_message,
+                "cover_image" => $item->link_image,
                 "parent" => $parent,
                 "total_engagement" => $item->total_engagement,
                 "number_of_shares" => $item->number_of_shares,
@@ -526,7 +528,7 @@ class MonitoringController extends Controller
                     "post_date" => Carbon::parse($item->message_datetime)->format('Y/m/d'),
                     "post_time" => Carbon::parse($item->message_datetime)->format('H:i'),
                     "icon" => "",
-                    "cover_image" => "",
+                    "cover_image" => $item->link_image,
                     "source_id" => $item->source_id,
                     "account_name" => $item->author,
                     "message_type" => $item->message_type,
@@ -549,7 +551,7 @@ class MonitoringController extends Controller
             "post_date" => Carbon::parse($post->message_datetime)->format('Y/m/d'),
             "post_time" => Carbon::parse($post->message_datetime)->format('H:i'),
             "icon" => "",
-            "cover_image" => "",
+            "cover_image" => $post->link_image,
             "source_id" => $post->source_id,
             "account_name" => $post->author,
             "message_type" => $post->message_type,
@@ -614,7 +616,6 @@ class MonitoringController extends Controller
                 $messages['neutral'] = round(($neutralCount / $totalSentiment) * 100, 2);
                 $messages['total_sentiment'] = $totalSentiment;
                 $messages["icon"] = "";
-                $messages["cover_image"] = "";
                 $messages["account_name"] = $messages['author'];
                 $messages["source_name"] = self::matchSourceName($source, $messages['source_id']);
                 unset($messages["classification"]);
@@ -732,7 +733,7 @@ class MonitoringController extends Controller
                 "post_time" => Carbon::parse($item->message_datetime)->format('H:i'),
                 "keyword_name" => self::matchKeywordName($keywords, $item->keyword_id),
                 "icon" => "",
-                "cover_image" => "",
+                "cover_image" => $item->link_image,
                 "message_type" => $item->message_type,
                 "device" => $item->device,
                 "source_name" => self::matchSourceName($source, $item->source_id),
@@ -827,7 +828,7 @@ class MonitoringController extends Controller
             $sourceQuery = "m.source_id IN (" . implode(",", $source_ids) . ") AND ";
         }
 
-        $query = "SELECT m.id,m.source_id,
+        $query = "SELECT m.id,m.source_id,m.link_image,
         m.author,m.message_id,m.message_type,m.message_datetime,m.message_id,m.number_of_comments,m.number_of_shares,m.number_of_reactions,mr.classification_id,m.reference_message_id
     FROM
         tbl_messages m
@@ -866,6 +867,7 @@ error_log  ($sourceQuery);
                         'negative' => 0,
                         'positive' => 0,
                         'neutral' => 0,
+                        'cover_image'=>"",
                         'total_sentiment' => 0,
                         'number_of_reactions' => 0,
                         'message_datetime' => '',
@@ -887,6 +889,7 @@ error_log  ($sourceQuery);
                         break;
                 }
 
+                $newGroupedData[$messageId]['cover_image'] = $message->link_image;
                 $newGroupedData[$messageId]['total_sentiment'] = $newGroupedData[$messageId]['total_sentiment'] + 1;
                 $newGroupedData[$messageId]['number_of_comments'] += $message->number_of_comments;
                 $newGroupedData[$messageId]['number_of_shares'] += $message->number_of_shares;
@@ -932,6 +935,7 @@ error_log  ($sourceQuery);
             $groupedResults[$author]['number_of_shares'] += $messageData['number_of_shares'];
             $groupedResults[$author]['number_of_reactions'] += $messageData['number_of_reactions'];
             $groupedResults[$author]['negative'] += $messageData['negative'];
+            $groupedResults[$author]['cover_image'] = $messageData['cover_image'];
             $groupedResults[$author]['neutral'] += $messageData['neutral'];
             $groupedResults[$author]['positive'] += $messageData['positive'];
             $groupedResults[$author]['total_sentiment'] += $messageData['total_sentiment'];
